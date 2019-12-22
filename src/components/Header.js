6512b4549ce595176d64032cf008bafb4412c0a1
MasonTDaniel <danielm@southwestern.edu>
2019-12-22 00:34:52 -0600

    header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import logo from "../svg/logo.svg";
 import { NavLink, Link } from "react-router-dom";
 import styled from "styled-components";
+import { ic_keyboard_arrow_right } from "react-icons-kit/md/ic_keyboard_arrow_right";
 
 class Header extends Component {
   render() {
@@ -15,7 +16,9 @@ class Header extends Component {
         </div>
         {/* Header Content */}
         <div className="header-content">
-          <h1></h1>
+          <Title>See what's next.</Title>
+          <Subtitle>WATCH ANYWHERE. CANCEL ANYTIME</Subtitle>
+          <Link className="main-offer-btn">try it now</Link>
         </div>
       </HeaderComponent>
     );
@@ -60,4 +63,55 @@ const HeaderComponent = styled.div`
     height: 10rem;
     z-index: 1;
   }
+
+  // Header Content
+  .header-content {
+    width: 65%;
+    position: relative;
+    margin: 4.5rem auto 0;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    text-align: center;
+    flex-direction: column;
+    z-index: 1;
+  }
+
+  // Main Offer Btn
+  .main-offer-btn {
+    display: inline-block;
+    background: var(--main-red);
+    text-transform: uppercase;
+    border: none;
+    outline: none;
+    margin: 0 33%;
+    padding: 1.5rem;
+    border-radius: 0.1875rem;
+    font-size: 2rem;
+    text-align: center;
+    box-shadow: 0 1px 0 rgba(0, 0, 0, 0.45);
+    transition: background 0.2s ease-in;
+    cursor: pointer;
+    &:hover {
+      background: var(--main-red-hover);
+    }
+  }
+`;
+
+// Main Title
+
+const Title = styled.h1`
+  margin: 0 0 1.2rem;
+  font-size: 5rem;
+  font-weight: 700;
+  line-height: 1.1em;
+`;
+
+// Subtitle
+const Subtitle = styled.h2`
+  font-weight: 400;
+  font-size: 1.875rem;
+  line-height: 1.25em;
+  margin: 0 0 1.875rem;
+  text-transform: uppercase;
 `;
