e30f894db3f7d1b20bb171b98fdfaaae421b8a58
MasonTDaniel <danielm@southwestern.edu>
2019-12-22 22:19:09 -0600

    added 2/3 tab components

--- src/components/TabComponent.js
+++ src/components/TabComponent.js
@@ -3,6 +3,8 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import TabDoor from "./tabs_nav/TabDoor";
 import TabDevices from "./tabs_nav/TabDevices";
 import TabPrices from "./tabs_nav/TabPrices";
+import TabContentOne from './TabContentOne';
+import TabContentTwo from './TabContentTwo';
 import "../css/TabsNav.css";
 
 class TabComponent extends Component {
@@ -21,7 +23,7 @@ class TabComponent extends Component {
             <Tab
               className={`${
                 this.state.tabIndex === 0 ? "tab-selected active" : null
-              }`}
+                }`}
             >
               <TabDoor />
               <p style={{ marginBottom: "1.875rem" }}>
@@ -34,7 +36,7 @@ class TabComponent extends Component {
             <Tab
               className={`${
                 this.state.tabIndex === 1 ? "tab-selected active" : null
-              }`}
+                }`}
             >
               <TabDevices />
               <p style={{ marginTop: "-5.3125rem" }}>
@@ -44,7 +46,7 @@ class TabComponent extends Component {
             <Tab
               className={`${
                 this.state.tabIndex === 2 ? "tab-selected active" : null
-              }`}
+                }`}
             >
               <TabPrices />
               <p>
@@ -52,6 +54,13 @@ class TabComponent extends Component {
               </p>
             </Tab>
           </TabList>
+          {/* Tabs Content */}
+          <TabPanel>
+            <TabContentOne />
+          </TabPanel>
+          <TabPanel>
+            <TabContentTwo />
+          </TabPanel>
         </Tabs>
       </div>
     );
