7db8327c73b1c031de796a466071d86441148032
MasonTDaniel <danielm@southwestern.edu>
2019-12-24 01:46:33 -0600

    Updates

---
+++ precache-manifest.e6e0700729800b2751c2ab7e3616a5cb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748d0a41b467e8db9d3239dff19930da",
+    "url": "/reactflix/index.html"
+  },
+  {
+    "revision": "6a28bb87447ec88855d6",
+    "url": "/reactflix/static/css/main.5d2486bf.chunk.css"
+  },
+  {
+    "revision": "ae3da0bcfc41d199fdf6",
+    "url": "/reactflix/static/js/2.b3608a50.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/reactflix/static/js/2.b3608a50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a28bb87447ec88855d6",
+    "url": "/reactflix/static/js/main.22a5a9b3.chunk.js"
+  },
+  {
+    "revision": "e4def9ddd06ac5bffae6",
+    "url": "/reactflix/static/js/runtime-main.690f6de7.js"
+  },
+  {
+    "revision": "d9c944025f233ba2bb9ef56e206ad924",
+    "url": "/reactflix/static/media/bg.d9c94402.jpg"
+  },
+  {
+    "revision": "db59947e8abc8ab2f9614b1e559d4f42",
+    "url": "/reactflix/static/media/logo.db59947e.svg"
+  },
+  {
+    "revision": "fab25bc378670766a10e1ec37712945e",
+    "url": "/reactflix/static/media/tab-1-pic.fab25bc3.png"
+  },
+  {
+    "revision": "66c7c1fb5b71b2deaff929bbcd3bacf7",
+    "url": "/reactflix/static/media/tab-macbook.66c7c1fb.png"
+  },
+  {
+    "revision": "dc986d819c053deb6180a34320af2988",
+    "url": "/reactflix/static/media/tab-tablet.dc986d81.png"
+  },
+  {
+    "revision": "9f91203b7c374a590dd727c323d5b6dc",
+    "url": "/reactflix/static/media/tab-tv.9f91203b.png"
+  }
+]);
